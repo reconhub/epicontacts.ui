9bd2785b0c738d01ce6f93d6a70bd4746c6d31b8
Dirk Schumacher <mail@dirk-schumacher.net>
2017-03-23 19:48:33 +0000

    Add ebola to default datasets

--- inst/epicontacts_server/global.R
+++ inst/epicontacts_server/global.R
@@ -1,2 +1,6 @@
-linelist_examples <- list(mers_korea_2015 = outbreaks::mers_korea_2015$linelist)
-contacts_examples <- list(mers_korea_2015 = outbreaks::mers_korea_2015$contacts)
+linelist_examples <- list(
+  mers_korea_2015 = outbreaks::mers_korea_2015$linelist,
+  ebola_sim = outbreaks::ebola_sim$linelist)
+contacts_examples <- list(
+  mers_korea_2015 = outbreaks::mers_korea_2015$contacts,
+  ebola_sim = outbreaks::ebola_sim$contacts)
