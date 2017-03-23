168980038fa3c3da6f3b6ee23fa48f7576d4c534
Dirk Schumacher <mail@dirk-schumacher.net>
2017-03-23 15:24:34 +0000

    Further improve the app

--- inst/epicontacts_server/server.R
+++ inst/epicontacts_server/server.R
@@ -4,54 +4,86 @@ library(visNetwork)
 library(ggplot2)
 library(shinyHelpers)
 # based on the original shiny app in package epicontacts
+
+# a variable to quickload data during development
+dev <- FALSE
+
+# converts types of a data.frame
+guess_coltypes <- function(df) {
+  if (!is.data.frame(df)) {
+    return(df)
+  }
+  for(col in colnames(df)) {
+    col_v <- df[[col]]
+    parser <- readr::guess_parser(col_v)
+    if (parser == "integer") {
+      df[[col]] <- as.integer(df[[col]])
+    }
+    if (parser == "date") {
+      df[[col]] <- lubridate::as_date(df[[col]])
+    }
+    if (parser == "double") {
+      df[[col]] <- as.numeric(df[[col]])
+    }
+  }
+  df
+}
 shinyServer(function(input, output, session) {
   
   linelist_data <- shinyHelpers::dataimportServer("import_linelist")
   contact_data <- shinyHelpers::dataimportServer("import_contact_data")
-  current_data <- reactive({
-    linelist <- tibble::as_tibble(linelist_data())
-    contacts <- tibble::as_tibble(contact_data())
+  
+  base_data <- reactive({
+    linelist <- guess_coltypes(linelist_data())
+    contacts <- guess_coltypes(contact_data())
     if (!is.null(linelist) && nrow(linelist) > 0 && 
         !is.null(contacts) && nrow(contacts) > 0) {
-      make_epicontacts(linelist, contacts, directed = TRUE)
+      make_epicontacts(linelist, contacts, directed = input$directed)
+    } else if (dev) {
+      make_epicontacts(outbreaks::mers_korea_2015$linelist, outbreaks::mers_korea_2015$contacts, directed = TRUE)      
     }
   })
   
-  subset_data <- eventReactive(input$subset, {
-    dat <- current_data()
-    
-    # build arguments for subsetting
-    subsetarglist <- list()
-    if (inherits(dat$linelist[, input$interact], "Date")) {
-      subsetarglist[[1]] <-
-        c(as.Date(input$dynamic[1]), as.Date(input$dynamic[2]))
+  current_data <- reactive({
+    dat <- base_data()
+    input$subset # trigger if clicked
+    if (input$subset_filter_data) {
+      # build arguments for subsetting
+      subsetarglist <- list()
+      interact <- isolate(input$interact)
+      dynamic <- isolate(input$dynamic)
+      if (inherits(dat$linelist[, interact], "Date")) {
+        subsetarglist[[1]] <-
+          c(as.Date(dynamic[1]), as.Date(dynamic[2]))
+      } else if (inherits(dat$linelist[, interact], "numeric")) {
+        subsetarglist[[1]] <- dynamic
+      } else {
+        subsetarglist[[1]] <- dynamic
+      }
+      names(subsetarglist)[1] <- interact
       
-    } else if (inherits(dat$linelist[, input$interact], "numeric")) {
-      subsetarglist[[1]] <- input$dynamic
+      # call epicontacts method for subsetting
+      subset(dat, node.attribute = subsetarglist)
     } else {
-      subsetarglist[[1]] <- input$dynamic
+      dat
     }
-    names(subsetarglist)[1] <- input$interact
-    
-    # call epicontacts method for subsetting
-    subset(dat, node.attribute = subsetarglist)
   })
   
   output$ui1 <- renderUI({
     # create list of attributes from linelist minus the id column
-    datcols <- names(current_data()$linelist)[-1]
+    dat <- base_data()$linelist
+    datcols <- colnames(dat)[-1]
     selectInput("interact", "Linelist Attributes", choices = datcols)
   })
   
   output$ui2 <- renderUI({
     req(input$interact)
     
-    dat <- current_data()$linelist
+    dat <- base_data()$linelist
     
     # define character of factor checking function
     factchar <- function(x) {
       is.character(x) | is.factor(x)
-      
     }
     
     # create list of input options based on class of columm
@@ -78,14 +110,7 @@ shinyServer(function(input, output, session) {
   
   output$netplot <- renderVisNetwork ({
     req(input$interact)
-    
-    if (input$subset) {
-      dat <- subset_data()
-    } else {
-      dat <- current_data()
-    }
-    
-    plot(dat,
+    plot(current_data(),
          annot = TRUE,
          editor = TRUE,
          group = input$interact)
@@ -94,61 +119,105 @@ shinyServer(function(input, output, session) {
   
   output$linelisttab <- DT::renderDataTable ({
     req(input$interact)
-    if (input$subset) {
-      subset_data()$linelist
-    } else {
-      current_data()$linelist
-    }
-    
+    current_data()$linelist
   })
   
   output$contactstab <- DT::renderDataTable ({
     req(input$interact)
-    if (input$subset) {
-      subset_data()$contacts
-    } else {
-      current_data()$contacts
-    }
+    current_data()$contacts
   })
   
   # pairwise
   
   valid_pairwise_cols <- reactive({
     unlist(Filter(function(x) {
-      col_value <- unlist(current_data()$linelist[[x]])
-      print(col_value)
+      col_value <- unlist(base_data()$linelist[[x]])
       (is.numeric(col_value) ||
           inherits(col_value, "Date") ||
           is.character(col_value)) && !all(is.na(col_value))
-    }, colnames(current_data()$linelist)[-1]))
+    }, colnames(base_data()$linelist)[-1]))
   })
   
   observe({
-    updateSelectInput(session, "pairwise_dist_histogram_col",
+    updateSelectInput(session, "pairwise_dist_col",
                       choices = valid_pairwise_cols())
   })
   
   pairwise_dist <- reactive({
-    column <- input$pairwise_dist_histogram_col
+    column <- input$pairwise_dist_col
     is_valid_column <- column %in% valid_pairwise_cols()
     if (is_valid_column) {
       get_pairwise(current_data(), column)
     }
   })
+  
   pairwise_plot_data <- reactive({
     data.frame(value = pairwise_dist(),
                stringsAsFactors = FALSE)
   })
+  
+  pairwise_dgamma_est <- reactive({
+    # distcrete gamma ML estimation based on Rich FitzJohn's distcrete package. License MIT 2016
+    data <- pairwise_dist()
+    data <- data[!is.na(data)]
+    deviance <- function(param) {
+      d <- distcrete("gamma", interval = 1L, param[1], param[2])$d
+      -2 * sum(d(data, log = TRUE))
+    }
+    optim(c(1,1), deviance)
+  })
+  
+  pairwise_dexp_est <- reactive({
+    # distcrete exp ML estimation based on Rich FitzJohn's distcrete package. License MIT 2016
+    data <- pairwise_dist()
+    data <- data[!is.na(data)]
+    ll <- function(param) {
+      d <- distcrete("exp", interval = 1L, param)$d
+      sum(d(data, log = TRUE))
+    }
+    optimise(ll, c(0, 20), maximum = TRUE)
+  })
+  
+  pairwise_density_fun <- reactive({
+    fun_key <- input$pairwise_est_density_fun
+    if (fun_key == "dgamma") {
+      est <- pairwise_dgamma_est()
+      distcrete("gamma", interval = 1L, est$par[1], est$par[2])$d
+    } else if (fun_key == "dexp") {
+      est <- pairwise_dexp_est()
+      distcrete("exp", interval = 1L, est$maximum)$d
+    }
+  })
+  
   output$pairwise_distribution_histogram <- renderPlot({
-    column <- input$pairwise_dist_histogram_col
+    column <- input$pairwise_dist_col
     plot_data <- pairwise_plot_data()
     mean_dist <- mean(plot_data$value, na.rm = TRUE)
-    ggplot(plot_data, aes(value)) +
-      geom_histogram(bins = input$pairwise_dist_histogram_bins) +
-      geom_vline(xintercept = mean_dist) +
+    p <- ggplot(plot_data, aes(value)) +
+      geom_vline(xintercept = mean_dist) + 
       xlab(column) +
       ggtitle(paste0("Histogram pairwise distances of column '", column, "'")) + 
       ylab("value")
+    if (input$pairwise_est_density) {
+      dgam <- pairwise_density_fun()
+      p <- p + 
+        stat_function(fun = dgam, colour = "red") +                    
+        geom_histogram(aes(y = ..density..), alpha = 0.4, bins = input$pairwise_dist_histogram_bins)
+    } else {
+      p <- p + geom_histogram(bins = input$pairwise_dist_histogram_bins)
+    }
+    p
+  })
+  
+  output$pairwise_dist_optim_output <- renderPrint({
+    if (input$pairwise_est_density) {
+      fun_key <- input$pairwise_est_density_fun
+      if (fun_key == "dgamma") {
+        pairwise_dgamma_est()
+      } else if (fun_key == "dexp") {
+        pairwise_dexp_est()
+      }
+    }
   })
   
   output$pairwise_sample_mean <- renderText({
@@ -160,7 +229,7 @@ shinyServer(function(input, output, session) {
   })
   
   output$pairwise_boxplot <- renderPlot({
-    column <- input$pairwise_dist_histogram_col
+    column <- input$pairwise_dist_col
     plot_data <- pairwise_plot_data()
     ggplot(plot_data, aes(value, value)) +
       geom_boxplot() +
--- inst/epicontacts_server/ui.R
+++ inst/epicontacts_server/ui.R
@@ -2,48 +2,67 @@ library(shiny)
 library(visNetwork)
 library(shiny)
 
-shinyUI(fluidPage(
-
-    titlePanel("Contact Tracing"),
-
-    sidebarLayout(
-        sidebarPanel(
-            shinyHelpers::dataimportUI("import_linelist", label = "Linelist data"),
-            shinyHelpers::dataimportUI("import_contact_data", label = "Contact data"),
-            checkboxInput("directed", "Is This A Directed Network ?", value = TRUE),
-            uiOutput("ui1"),
-            uiOutput("ui2"),
-            actionButton("subset", "Subset Data Based on Inputs Above")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            tabsetPanel(
-                tabPanel('Line List',
-                    DT::dataTableOutput("linelisttab")),
-                tabPanel('Contact List',
-                    DT::dataTableOutput("contactstab")),
-                tabPanel(
-                  "Pairwise viewer",
-                  fluidRow(
-                    column(12, 
-                      selectInput("pairwise_dist_histogram_col", "Linelist attribute", choices = character(0)),
-                      sliderInput("pairwise_dist_histogram_bins", "Number of bins", min = 5, max = 100, value = 30))),
-                  fluidRow(
-                    column(6, plotOutput("pairwise_distribution_histogram")),
-                    column(6, plotOutput("pairwise_boxplot"))
-                  ),
-                  hr(),
-                  p(
-                    "Sample mean: ", textOutput("pairwise_sample_mean", inline = TRUE)
-                  ),
-                  p(
-                    "Sample standard deviation: ", textOutput("pairwise_sample_sd", inline = TRUE)
-                  )
-                ),
-                tabPanel('Network plot', 
-                         visNetworkOutput("netplot"))
-            )
-        )
+shinyUI(navbarPage(
+  "EpiContacts.ui",
+  tabPanel(
+    "Data",
+    fluidRow(
+      column(6,
+             h2("Load data"),
+             h3("Linelist data"),
+             shinyHelpers::dataimportUI("import_linelist"),
+             h3("Contact data"),
+             shinyHelpers::dataimportUI("import_contact_data"),
+             checkboxInput("directed", "Is This A Directed Network ?", value = TRUE)),
+      column(6,
+             h2("Subset data"),
+             checkboxInput("subset_filter_data", "Enable subsetting", value = FALSE),
+             uiOutput("ui1"),
+             uiOutput("ui2"),
+             actionButton("subset", "Subset Data Based on Inputs Above"))
     )
+  ),
+  tabPanel("Linelist",
+           DT::dataTableOutput("linelisttab")),
+  tabPanel("Contact List",
+           DT::dataTableOutput("contactstab")),
+  tabPanel(
+    "Pairwise viewer",
+    fluidRow(column(
+      12,
+      selectInput(
+        "pairwise_dist_col",
+        "Linelist attribute",
+        choices = character(0)
+      ),
+      sliderInput(
+        "pairwise_dist_histogram_bins",
+        "Number of bins",
+        min = 5,
+        max = 100,
+        value = 30
+      )
+    )),
+    fluidRow(column(
+      6, plotOutput("pairwise_distribution_histogram")
+    ),
+    column(6, plotOutput("pairwise_boxplot"))),
+    hr(),
+    p(
+      "Sample mean: ",
+      textOutput("pairwise_sample_mean", inline = TRUE)
+    ),
+    p(
+      "Sample standard deviation: ",
+      textOutput("pairwise_sample_sd", inline = TRUE)
+    ),
+    hr(),
+    h3("Estimate distribution parameters"),
+    checkboxInput("pairwise_est_density", "Estimate a distribution parameters", value = FALSE),
+    selectInput("pairwise_est_density_fun", "Distribution", choices = c("Discretized Exponential" = "dexp", 
+                                                    "Discretized Gamma" = "dgamma")),
+    verbatimTextOutput("pairwise_dist_optim_output")
+  ),
+  tabPanel("Network plot",
+           visNetworkOutput("netplot"))
 ))
